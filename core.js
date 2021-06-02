c1b3e87cd1cd06b03a650b3004e4bef8c5b249d1
Le314u <lucasmfpiu@gmail.com>
2021-06-02 13:24:57 -0300

    Bootstrap  e ideia Geral

--- core.js
+++ core.js
@@ -1,10 +1,3 @@
-# megasena-prolog
-
-
-<h3>Lucas Mateus Fernandes 0035411</h3>
-<h3>Pedro Daniel Camargos Soares 0020640</h3>
-
-
 // Criar ambiente Prolog do tu vasa
 // Carregar arquivo fonte
 // Fromatar arquivo fonte
